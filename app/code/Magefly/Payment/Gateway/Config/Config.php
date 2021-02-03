e25d8e819c9e2a1fbe2465006b62ef90c21200f7
Mehmet Emin Uygur <meminuygur@yandex.com>
2021-02-03 16:04:31 +0300

    config

--- app/code/Magefly/Payment/Gateway/Config/Config.php
+++ app/code/Magefly/Payment/Gateway/Config/Config.php
@@ -3,7 +3,6 @@ namespace Magefly\Payment\Gateway\Config;
 
 class Config extends \Magento\Payment\Gateway\Config\Config
 {
-
     public function isActive()
     {
         return true;
@@ -12,5 +11,62 @@ class Config extends \Magento\Payment\Gateway\Config\Config
     public function isCvvEnabled()
     {
         return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSandbox()
+    {
+        return ($this->getValue('environment') === 'sandbox');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEndpointUrl()
+    {
+        return $this->getEnvValue('endpoint');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClientId()
+    {
+        return $this->getEnvValue('client_id');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClientPassword()
+    {
+        return $this->getEnvValue('client_password');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getClientUserName()
+    {
+        return $this->getEnvValue('client_username');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOrderHistoryEndpoint()
+    {
+        return $this->getEnvValue('order_history_endpoint');
+    }
+
+    /**
+     * @param string $field
+     * @return mixed|null
+     */
+    protected function getEnvValue(string $field)
+    {
+        return $this->getValue(\sprintf('%s_%s', $this->getValue('environment'), $field));
     }
 }
